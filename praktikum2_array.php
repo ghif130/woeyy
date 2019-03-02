9c51cf6c647527ac4b6386c9ff5dc51b3e6ccaf0
ghif130 <ghifariabdillah01@gmail.com>
2019-03-02 12:00:37 +0700

    upload pertama

---
+++ praktikum2_array.php
@@ -0,0 +1,44 @@
+<?php
+$arrayName = array("maryam","culis");
+
+$mahasiswa = array(
+array(
+"nama" => "maryam",
+"nim" => "001",
+"kampus" => "uin"),
+array(
+"nama" => "agus",
+"nim" => "002",
+"kampus" => "uin")
+);
+
+print_r($mahasiswa);
+echo $mahasiswa[0]["nama"]."<br><br>";
+echo $mahasiswa[1]["nim"]."<br><br>";
+
+
+for ($i=0;$i<2;$i++){
+echo $arrayName[$i]."<br><br>";
+}
+
+for ($e=0;$e<2;$e++){
+	foreach ($mahasiswa[$e] as $key => $value){
+		echo $mahasiswa[$e][$key]."<br><br>";
+	}
+}
+
+$nama = "10a";
+if(empty($nama) || $nama == "" || $nama === ''){
+	echo "kosong"."<br><br>";
+}else{
+	echo "ada";
+	echo " yaitu ".$nama."<br><br>";}
+
+$suhu = 30;
+if($suhu == 30){
+	echo "suhu normal"."<br>" ;
+}else if($suhu < 30){
+	echo "suhu dingin"."<br>";
+}else{echo "panas";}
+
+?>
