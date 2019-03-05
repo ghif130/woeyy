fd4f3c34d3c358d93900b38a5eea94ca91179cdc
ghif130 <ghifariabdillah01@gmail.com>
2019-03-05 15:11:28 +0700

    Praktikum 3

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Judul</title>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<style type="text/css">
+	*{
+		margin: 0;
+	}
+</style>
+</head>
+<body>
+<div class="col-md-12" style="background-color: red; height: 50px; margin-bottom: 20px;">
+	
+</div>
+
+<div class="col-md-12" style="background-color: #ecf0f1; height: 200px; padding: 5px;">
+	<div class="col-md-4" style="height:180px; background-color:grey"></div>
+	<div class="col-md-7" style="height:180px; background-color:white">
+			<div class="col-md-12" style="margin:2px;">
+				<h4>This is title</h4>
+				<p>Description</p>
+			</div>
+
+	</div>
+</div>
+
+</body>
+</html>
